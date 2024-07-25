eb22a923a513b0bf13ace9a668ecb461ccee3289
Huovinen Kasperi <kasperi.huovinen@outlook.com>
2024-07-25 15:34:53 +0300

    Added weather save question y/n

--- weather_app.py
+++ weather_app.py
@@ -3,8 +3,13 @@ import json
 
 # Saving previously checked weather into json file
 def saveWeather(data):
-    with open('savedWeather.json', 'w') as new_file:
-        json.dump(data, new_file, indent=4)
+    save = input("\nWould you like to save the weather information? y/n? ")
+    if save == 'y':
+        with open('savedWeather.json', 'w') as new_file:
+            json.dump(data, new_file, indent=4)
+            print("Weather infromation saved to: savedWeather.json")
+    else:
+        print("Not saved")
 
 def get_weather(api_key, city):
     base_url = "http://api.openweathermap.org/data/2.5/weather"
@@ -15,9 +20,9 @@ def get_weather(api_key, city):
     }
     
     response = requests.get(base_url, params=params)
-    
+
+
     if response.status_code == 200:
-        saveWeather(response.json())
         return response.json()
     else:
         print(response)
@@ -28,10 +33,15 @@ if __name__ == "__main__":
     api_key = "1ab43bda6c8fec81a4552e949b28969e"
     city = input("Enter the city name: ")
     weather_data = get_weather(api_key, city)
+
     
     if weather_data:
         print(f"City: {weather_data['name']}")
         print(f"Temperature: {weather_data['main']['temp']}°C")
         print(f"Weather: {weather_data['weather'][0]['description']}")
+        saveWeather(weather_data)
     else:
         print("Error fetching the weather data.")
+
+
+
